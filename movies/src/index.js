faed3bf728bbc99f3e3c16aef477844efa695f11
theopakieser <20101918@mail.wit.ie>
2024-10-17 16:15:36 +0100

    full review page

--- movies/src/index.js
+++ movies/src/index.js
@@ -22,6 +22,7 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
       </Routes>
     </BrowserRouter>
   );
